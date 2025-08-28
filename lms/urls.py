2f8f3afd38c1e9ff0fdbaf7535e54a5b10dc5a8b
ANTAQ <mymillions@ya.ru>
2025-08-28 14:30:28 +0300

    добавлен namespace в include в config/urls и app_name в urls приложений

--- lms/urls.py
+++ lms/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CourseViewSet, LessonListCreateView, LessonDetailView
 
+app_name = 'lms'
+
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='course')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include((router.urls, app_name))),
     path('lessons/', LessonListCreateView.as_view(), name='lesson-list-create'),
     path('lessons/<int:pk>/', LessonDetailView.as_view(), name='lesson-detail'),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import UserCreateView, UserDetailView
 
+app_name = 'users'
+
 urlpatterns = [
     path('register/', UserCreateView.as_view(), name='user-register'),
     path('<int:pk>/', UserDetailView.as_view(), name='user-detail'),
